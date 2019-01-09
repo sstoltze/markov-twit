c8d6866aa1733c48094d32ab54ac64dbf68fe9db
sstoltze <sstoltze@gmail.com>
2019-01-09 22:16:21 +0100

    Integrate twitter
    Still need to fix issue with 'nil' appearing as a key, resulting in
    empty tweets

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
-  :main ^:skip-aot markov-twit.core
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [twitter-api "1.8.0"]
+                 [environ "1.1.0"]]
+  :plugins [[lein-environ "1.1.0"]]
+  :main ^:skip-aot markov-twit.generator
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/markov_twit/generator.clj
+++ src/markov_twit/generator.clj
@@ -1,9 +1,18 @@
 ;; Markov chains: http://howistart.org/posts/clojure/1/
 ;; Twitter:
 (ns markov-twit.generator
-  (:gen-class))
+  (:gen-class)
+  (:require
+   [twitter.api.restful :as twitter]
+   [twitter.oauth :as twitter-oauth]
+   [environ.core :refer [env]]))
 
 (def example "And the Golden Grouse And the Pobble who")
+
+(def credentials (twitter-oauth/make-oauth-creds (env :app-consumer-key)
+                                                 (env :app-consumer-secret)
+                                                 (env :user-access-token)
+                                                 (env :user-access-secret)))
 
 (defn word-chain [transitions]
   (let [n (reduce max (map count transitions))]
@@ -11,9 +20,10 @@
               (merge-with clojure.set/union
                           r
                           {(cond
-                             (< (count t) n) (concat (butlast t)
-                                                     (take (- n (count t))
-                                                           (repeat nil)))
+                             (< (count t) n) #{}
+                             ;; (concat (butlast t)
+                             ;;                         (take (- n (count t))
+                             ;;                               (repeat nil)))
                              :else (butlast t))
                            #{(last t)}}))
             {}
@@ -30,7 +40,7 @@
 (defn word-list->text [chain]
   (apply str (interpose " " chain)))
 
-(defn walk-chain [chain prefix result min-length]
+(defn walk-chain [chain prefix result max-length]
   (let [suffixes (get chain prefix)]
     (if (empty? suffixes)
       result
@@ -38,22 +48,45 @@
             new-prefix (concat (rest prefix) [suffix])
             new-suffix (concat result [suffix])
             text-length (count (word-list->text new-suffix))]
-        (if (>= text-length min-length)
-          new-suffix
-          (recur chain new-prefix new-suffix min-length))))))
+        (if (>= text-length max-length)
+          result
+          (recur chain new-prefix new-suffix max-length))))))
 
 (defn generate-text
-  ([chain start-phrase min-length]
-   (let [prefix (clojure.string/split start-phrase #" ")]
-     (word-list->text (walk-chain chain prefix prefix min-length))))
+  ([chain]
+   (generate-text chain (word-list->text (first (first chain)))))
   ([chain start-phrase]
-   (generate-text chain start-phrase 280)))
+   (generate-text chain start-phrase 280))
+  ([chain start-phrase max-length]
+   (let [prefix (clojure.string/split start-phrase #" ")]
+     (word-list->text (walk-chain chain prefix prefix max-length)))))
 
 (defn file->word-chain
-  ([file-name chain-length]
-   (text->word-chain (slurp file-name) chain-length))
   ([file-name]
-   (text->word-chain (slurp file-name))))
+   (text->word-chain (slurp file-name)))
+  ([file-name chain-length]
+   (text->word-chain (slurp file-name) chain-length)))
 
+(defn clean-text [text]
+  (let [trimmed (if (re-find #"[.,!?]" text)
+                  (apply str (re-seq #"[\s\w]+[^.!?,]*[.!?,]" text)) ; Trim to last punctuation
+                  (apply str (re-seq #".*[^a-zA-Z]+" text))) ; Trim last word
+        cleaned (clojure.string/replace trimmed #"[,| ]$" ".")]
+    (clojure.string/replace cleaned #"\"" "'")))
+
+;; Max 200 tweets returned by Twitter api
+(defn tweets [user]
+  (->> (twitter/statuses-user-timeline :oauth-creds credentials :params {:screen-name user :count 200})
+       :body
+       (map :text)
+       (map clojure.string/lower-case))
+  ;(map clojure.string/lower-case (map :text (:body (twitter/statuses-user-timeline :oauth-creds credentials :params {:screen-name user :count 200}))))
+  )
+
+(defn list->chain [list]
+  (apply merge-with clojure.set/union (map text->word-chain list)))
+
+(defn generate-tweet [user]
+  (generate-text (list->chain (tweets user))))
 ;; To process several files:
 ;; (apply merge-with clojure.set/union (map file->word-chain files))
